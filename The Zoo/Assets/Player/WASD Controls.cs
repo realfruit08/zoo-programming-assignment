fd92cae00bcfb6358017fbf2e987c0cc4e2fdc23
realfruit08 <mick.mcnulty08@gmail.com>
2025-02-05 13:33:07 -0500

    Fixed wasd

--- The Zoo/Assets/Player/WASD Controls.cs
+++ The Zoo/Assets/Player/WASD Controls.cs
@@ -2,29 +2,39 @@ using UnityEngine;
 
 public class WASDControls : MonoBehaviour
 {
+    private SpriteRenderer spriteRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float movespeed = 5;
+        float sprint = 10f;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movespeed = sprint;
+        }
         Input.GetKey(KeyCode.W);
         Input.GetKey(KeyCode.A);
         Input.GetKey(KeyCode.S);
         Input.GetKey(KeyCode.D);
+        Input.GetKey(KeyCode.LeftShift);
 
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.up * Time.deltaTime * movespeed;
+          
         }
         if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * Time.deltaTime * movespeed;
+            spriteRenderer.flipX = false;
         }
+       
         if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.down * Time.deltaTime * movespeed;
@@ -32,6 +42,8 @@ public class WASDControls : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * Time.deltaTime * movespeed;
+            spriteRenderer.flipX = true;
         }
+       
     }
 }
